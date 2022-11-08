2604cf3266c11a4a8d333dd10cd4c061c55f2aa5
Calvin <calvin@larimo.re>
2022-11-08 19:34:29 +0000

    add networknamelength const

--- openttd/const.go
+++ openttd/const.go
@@ -1,8 +1,9 @@
 package openttd
 
 const (
-	gameVersion      = "12.2"
-	gameVersionMajor = 12
-	gameVersionMinor = 2
-	newGRFRevision   = (uint32(gameVersionMajor+16) << 24) | (uint32(gameVersionMinor) << 20) | (0x01 << 19) | 28004
+	gameVersion       = "12.2"
+	gameVersionMajor  = 12
+	gameVersionMinor  = 2
+	newGRFRevision    = (uint32(gameVersionMajor+16) << 24) | (uint32(gameVersionMinor) << 20) | (0x01 << 19) | 28004
+	networkNameLength = uint(80)
 )
