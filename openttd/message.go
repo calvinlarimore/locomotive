409f95c162e41b373d7829cc49c3af5d205704f2
Calvin <github@giantfroje.dev>
2022-11-08 00:36:19 +0000

    Add more message types

--- openttd/message.go
+++ openttd/message.go
@@ -4,19 +4,39 @@ type ServerMessage interface {
 	Type() byte
 }
 
+type ClientMessage interface {
+	Type() byte
+	packet() *Packet
+}
+
 type MessageHandler[T ServerMessage] interface {
 	Handle(T)
 }
 
 var messageHandlers = make(map[string]MessageHandler[ServerMessage])
 
+// Messages:
+
+// PACKET_SERVER_FULL
+type MessageServerFull struct{}
+
+func (m *MessageServerFull) Type() byte { return 0x00 }
+
+func createMessageServerFull(p *Packet) *MessageServerFull { return &MessageServerFull{} }
+
+// PACKET_SERVER_BANNED
+type MessageServerBanned struct{}
+
+func (m *MessageServerBanned) Type() byte { return 0x01 }
+
+func createMessageServerBanned(p *Packet) *MessageServerBanned { return &MessageServerBanned{} }
+
+// PACKET_SERVER_ERROR
 type MessageServerError struct {
 	Error byte
 }
 
-func (m *MessageServerError) Type() byte {
-	return 0x03
-}
+func (m *MessageServerError) Type() byte { return 0x03 }
 
 func createMessageServerError(p *Packet) *MessageServerError {
 	m := MessageServerError{}
