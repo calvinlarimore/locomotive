e0320132cc92e096c7d0e6188dd873098c3fe251
Calvin <github@giantfroje.dev>
2022-11-08 11:18:22 +0000

    Add some constants

---
+++ openttd/const.go
@@ -0,0 +1,8 @@
+package openttd
+
+const (
+	gameVersion      = "12.2"
+	gameVersionMajor = 12
+	gameVersionMinor = 2
+	newGRFRevision   = (uint32(gameVersionMajor+16) << 24) | (uint32(gameVersionMinor) << 20) | (0x01 << 19) | 28004
+)
