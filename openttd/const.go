de3ba73c91927a7dda8bc3d4b800ced569c9f15d
Calvin <calvin@larimo.re>
2022-11-12 06:52:08 +0000

    Add networkServerIDLength const

--- openttd/const.go
+++ openttd/const.go
@@ -1,10 +1,11 @@
 package openttd
 
 const (
-	gameVersion          = "12.2"
-	gameVersionMajor     = 12
-	gameVersionMinor     = 2
-	newGRFRevision       = (uint32(gameVersionMajor+16) << 24) | (uint32(gameVersionMinor) << 20) | (0x01 << 19) | 28004
-	networkNameLength    = uint(80)
-	networkVersionLength = uint(33)
+	gameVersion           = "12.2"
+	gameVersionMajor      = 12
+	gameVersionMinor      = 2
+	newGRFRevision        = (uint32(gameVersionMajor+16) << 24) | (uint32(gameVersionMinor) << 20) | (0x01 << 19) | 28004
+	networkNameLength     = uint(80)
+	networkVersionLength  = uint(33)
+	networkServerIDLength = uint(33)
 )
